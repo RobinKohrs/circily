659e2ecfcc1d4b18ed474a3422f23708e262389d
RobinKohrs <robin.kohrs@gmx.de>
2023-04-02 11:54:46 +0200

    initial commit

---
+++ src/lib/Search.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { onMount } from "svelte";
+  import Fuse from "fuse.js";
+
+  export let searchable;
+  export let options;
+
+  let searchbar;
+
+  //   let fuse;
+  //   $: if (searchbar && searchable && options) {
+  //     fuse = new Fuse(searchable, options);
+  //   }
+
+  let path = "https://storage.googleapis.com/gdatark/AT/AT10101.geojson";
+
+  onMount(async () => {
+    console.log("mounting");
+    const response = await fetch(path, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        // cors allows us to make requests to other domains
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+    const data = await response.json();
+    console.log("data", data);
+  });
+</script>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { onMount } from "svelte";
+
+  import * as topojson from "topojson-client";
+  import { csv } from "d3";
+  import { geoPath, geoAlbers, geoMercator } from "d3-geo";
+  import { draw } from "svelte/transition";
+
+  import Search from "$lib/Search.svelte";
+</script>
+
+<Search options={{ keys: ["gem_id"] }} />
+
+<style lang="scss">
+  .content-container {
+    margin: 0 5%;
+    height: 100%;
+    display: grid;
+    align-items: center;
+
+    & .gemeinde-container {
+      width: 400px;
+      height: 400px;
+      border: 1px solid black;
+    }
+  }
+</style>
