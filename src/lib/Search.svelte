8aff6ae07817a467433488f5c00d3d6e7e601947
RobinKohrs <robin.kohrs@gmx.de>
2023-04-02 14:13:19 +0200

    include countries

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -7,24 +7,10 @@
 
   let searchbar;
 
-  //   let fuse;
-  //   $: if (searchbar && searchable && options) {
-  //     fuse = new Fuse(searchable, options);
-  //   }
-
-  let path = "https://storage.googleapis.com/gdatark/AT/AT10101.geojson";
-
-  onMount(async () => {
-    console.log("mounting");
-    const response = await fetch(path, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        // cors allows us to make requests to other domains
-        "Access-Control-Allow-Origin": "*",
-      },
-    });
-    const data = await response.json();
-    console.log("data", data);
-  });
+  let fuse;
+  $: if (searchbar && searchable && options) {
+    fuse = new Fuse(searchable, options);
+  }
 </script>
+
+<input type="text" name="" id="" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,22 +6,90 @@
   import { geoPath, geoAlbers, geoMercator } from "d3-geo";
   import { draw } from "svelte/transition";
 
+  // custom components
   import Search from "$lib/Search.svelte";
+
+  // import data overview
+  import countries from "../assets/data/countries.json";
+
+  // variables
+  let selectedCountries = [];
+  let selectedGemeinden = [];
+  $: console.log("selectedGemeinden: ", selectedGemeinden);
+  function selectCountry(country) {
+    if (country === "All") {
+      if (selectedCountries.length === countries.length) {
+        selectedCountries = [];
+        return;
+      } else {
+        selectedCountries = countries.map((e) => e.country_id);
+      }
+    } else {
+      if (selectedCountries.includes(country)) {
+        selectedCountries = selectedCountries.filter((c) => c !== country);
+        return;
+      } else {
+        selectedCountries = [...selectedCountries, country];
+      }
+    }
+  }
+
+  async function loadCountries() {
+    let promises = selectedCountries.map(async (c) => {
+      const raw = await fetch("/data/" + c + ".json");
+      const json = await raw.json();
+      return { [c]: json };
+    });
+
+    Promise.all(promises).then((values) => {
+      selectedGemeinden = values;
+    });
+  }
+
+  $: if (selectedCountries) {
+    loadCountries();
+  }
+
+  onMount(() => {
+    selectCountry("AT");
+  });
 </script>
 
-<Search options={{ keys: ["gem_id"] }} />
+<div class="container-app">
+  <div class="container-countries flex gap-3 justify-center flex-wrap">
+    <button
+      class="p-2 rounded-md mr-auto bg-slate-400"
+      on:click={() => selectCountry("All")}
+    >
+      {#if selectedCountries.length === countries.length}
+        None
+      {:else}
+        All
+      {/if}
+    </button>
+    {#each countries as c, i}
+      <button
+        style:background-color={selectedCountries.includes(c.country_id)
+          ? "var(--btn-color)"
+          : "var(--btn-color-select)"}
+        class="p-2 rounded-md"
+        on:click={() => selectCountry(c.country_id)}
+      >
+        {c.country_id}
+      </button>
+    {/each}
+  </div>
 
+  <Search />
+</div>
+
+<!-- <Search options={{ keys: ["gem_id"] }} /> -->
 <style lang="scss">
-  .content-container {
-    margin: 0 5%;
+  .container-app {
     height: 100%;
-    display: grid;
-    align-items: center;
-
-    & .gemeinde-container {
-      width: 400px;
-      height: 400px;
-      border: 1px solid black;
-    }
+    max-width: 650px;
+    margin: 0 auto;
+    border: 1px solid black;
+    position: relative;
   }
 </style>
